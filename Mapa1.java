1cbb77262f8130493d98ef126692c3474bd5cc47
thiagotoreto <thiagotoretodamaceno@gmail.com>
2025-05-29 22:39:25 -0300

    teste barril rolando

--- Mapa1.java
+++ Mapa1.java
@@ -5,9 +5,9 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.File;
 import java.lang.Thread;
-import java.lang.reflect.InvocationTargetException;
+
 public class Mapa1 extends JFrame {
-   Mapa1(){
+   Mapa1(){ 
     JFrame frame = new JFrame(); 
     frame.setSize(560,480);    
     frame.add(new Imagens());
@@ -24,17 +24,16 @@ public class Mapa1 extends JFrame {
         BufferedImage princess;
         BufferedImage mario;
         BufferedImage kong;
-        BufferedImage barril;
-        int i =0;
+        int i = 0, j = 0;
+        int dx = 148, dy = 105;
         
         Image[] sprites = new Image[5];
+        Image[] barril = new Image[2];
         Imagens(){
             try{
                 map1 = javax.imageio.ImageIO.read(new java.io.File("sprites/spritesm1.png"));
                 princess = javax.imageio.ImageIO.read(new java.io.File("sprites/princess.png"));
                 mario = javax.imageio.ImageIO.read(new java.io.File("sprites/m1.png"));
-                barril = javax.imageio.ImageIO.read(new java.io.File("sprites/b1.png"));
-                new Thread(this).start();
                 new Thread(this).start();
             }catch(java.io.IOException e){
                 e.printStackTrace();
@@ -45,6 +44,8 @@ public class Mapa1 extends JFrame {
             sprites[2] = ImageIO.read(new File("sprites/d3.png"));
             sprites[3] = ImageIO.read(new File("sprites/d5.png"));
             sprites[4] = ImageIO.read(new File("sprites/d6.png"));
+            barril[0] = ImageIO.read(new File("sprites/b1__2_-removebg-preview.png"));
+            barril[1] = ImageIO.read(new File("sprites/b2-removebg-preview.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }    
@@ -54,24 +55,29 @@ public class Mapa1 extends JFrame {
         g.drawImage(map1, 0, 0, 550, 450,null);
         g.drawImage(princess, 240,32, 50,50, null);
         g.drawImage(mario, 90, 400, 30, 30, null);
-        g.drawImage(barril, 9, 400, 30, 30, null);
         if(i!=3)
             g.drawImage(sprites[i], 50, 35,100,100,null);
         else
             g.drawImage(sprites[i], 50, 50,100,100,null);
+        g.drawImage(barril[j], dx, dy, 25, 25, null);
     }
     public void run() {
                 while(true){
                     i+=1;
+                    j+=1;
+                    dx+=5;
+                    dy+=1;
                     repaint();
                     if(i==5)
                         i=0;
+                    if(j==2)
+                        j = 0;
                     try{
                         Thread.sleep(1000);
-                         }
-                     catch(Exception e){
-                         e.printStackTrace();
-              }
+                    }
+                    catch(Exception e){
+                        e.printStackTrace();
+                    }
+                }
             }
-}
     }
